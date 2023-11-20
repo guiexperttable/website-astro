aaff30b18a7a2a84f86c7cf46b0b82138c000770
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-11-20 14:47:51 +0100

    demo proto

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [
-    tailwind()
+    tailwind(),
   ],
   vite: {
     ssr: {
