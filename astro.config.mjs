62637aebffdf36721eba33936549eb4c032f3204
Marc Kronberg <krocon@gmail.com>
2023-11-23 15:30:38 +0100

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,21 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import astroExpressiveCode from 'astro-expressive-code'
+import astroExpressiveCode from 'astro-expressive-code';
 
 /** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+import sitemap from "@astrojs/sitemap";
 const astroExpressiveCodeOptions = {
   // Example: Change the themes
-  themes: ['github-dark'],
-}
+  themes: ['github-dark']
+};
 
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    // expressiveCode(),
-    astroExpressiveCode(astroExpressiveCodeOptions),
-  ],
+  site: "https://gui.expert/",
+  integrations: [tailwind(),
+  astroExpressiveCode(astroExpressiveCodeOptions), sitemap()],
   vite: {
     ssr: {
       external: ["svgo"],
