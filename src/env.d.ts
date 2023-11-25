97689f2edf754afc0fac9d9e27ed82f7f6d9f991
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-11-25 19:06:28 +0100

    config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="../public/scripts/table/index.d.ts" />
+/// <reference types="../public/scripts/demo-table-models/index.d.ts" />
